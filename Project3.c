17fdcf8a1a97ba61c069d886bd84b5d7a1781cf2
Spencer6497 <peaces2@mymail.nku.edu>
2019-10-10 20:35:48 -0400

    Includes, constants, seed RNG

--- Project3.c
+++ Project3.c
@@ -12,11 +12,32 @@
  * Project3
  */
 
+// Include IO functions for file output
 #include <stdio.h>
 
+// Include functions for random number generation & seeding RNG
+#include <stdlib.h>
+#include <time.h>
+
+// Define constant for size of the board
+const int SIZE = 100;
+
+// Main function, responsible for initializing players, board, and looping until a player wins, also outputs result
 void main() {
     // Initialize board array
     char board[100]="  mHk nH l B He Flq p H  hByHlho H B  jr HFB ir j H  F ku gd  H pjB mH x  BF i H  m oB HlHFBhoH BB ";
+
+    // Seed random number generator
+    srand(time(NULL));
+
     // Initialize both player pointers to the beginning of the array
     char *p1, *p2 = board;
+
+    // Initialize file pointer for the output textfile
+    FILE *fp1;
+    fp1 = fopen("output.txt", "w");
+
+    // Test
+
 }
+
