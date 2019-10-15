398996c5b91cb0b944fa3077459d78bd58eae80e
Spencer6497 <peaces2@mymail.nku.edu>
2019-10-15 16:11:51 -0400

    Finish output function, verify functionality

--- Project3.c
+++ Project3.c
@@ -13,8 +13,8 @@
  */
 
 /* Function Prototypes
- *int move(char *, char *, char[] *, int, int);
- *void output(char[], char, char, FILE *);
+ *char * move(char *, char *, char[] *, int, int);
+ *void output(char[], char *, char *, FILE *);
  *char findHaven(char);
  *
  *
@@ -31,44 +31,54 @@
 const int SIZE = 100;
 
 // Define function to output the current game board to disk file on a single line
-void output(char board[], char *p1, char p2, FILE *fp1) {
+void output(char board[], char *p1, char *p2, FILE *fp1) {
     char *a = board; // Character pointer used to iterate through board
     // Iterate through the board
     while(*a != '\0') {
         // If current position is player 1, print 1
         if (a == p1) {
-            putchar(1);
+            putc('1', fp1);
+        // If current position is player 2, print 2
+        } else if (a == p2) {
+            putc('2', fp1);
+        // Otherwise, print whatever is at the current position
+        } else {
+            putc(*a, fp1);
         }
-        putchar(*a);
+        // Move the current position along
         a++;
+    }
+}
+
+// Define function to find the nearest haven, given the direction either forwards or backwards
+char findHaven(char direction) {
+    if (direction == 'B') {
+
+    } else if (direction == 'F') {
+
     }
 }
 
 // Main function, responsible for initializing players, board, and looping until a player wins, also outputs result
 void main() {
     // Initialize board array
-    char board[100]="  mHk nH l B He Flq p H  hByHlho H B  jr HFB ir j H  F ku gd  H pjB mH x  BF i H  m oB HlHFBhoH BB ";
+    char board[100]="  mHk  H l B He Flq p H  hByHlho H B  jr HFB ir j H  F ku gd  H pjB mH x  BF i H  m oB HlHFBhoH BB ";
 
     // Seed random number generator
     srand(time(NULL));
 
     // Initialize both player pointers to the beginning of the array
-    char *p1 = &board[2], *p2 = board;
+    char *p1 = board, *p2 = board;
 
     // Initialize file pointer for the output textfile
     FILE *fp1;
     fp1 = fopen("output.txt", "w");
 
     // Test
-    output(board, &p1, p2, fp1);
+    //output(board, p1, p2, fp1);
 }
 
-// Define move function to move players
-int move(char *p1, char *p2, char *board[], int playerNum, int size) {
-
-}
-
-// Define function to find the nearest haven, given the direction either forwards or backwards
-char findHaven(char direction) {
+// Define move function to move players, return pointer
+char * move(char *p1, char *p2, int playerNum, char *board[], int size) {
 
 }
